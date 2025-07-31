d89b527b64ed8848665ceff57b5166c52a9a8573
Rabbit49 <akasagarbha9@naver.com>
2025-07-31 22:31:22 +0900

    타일 관리 형태를 바꿈으로서 코드 간략화

--- EasyTileGame/Assets/Scripts/MapScripts/MapManager.cs
+++ EasyTileGame/Assets/Scripts/MapScripts/MapManager.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] private GameObject tileParent; // 해당 오브젝트 내 자식오브젝트. 타일 오브젝트들은 해당 오브젝트의 자식임
+    [SerializeField] private GameObject tileParent; // 해당 오브젝트 내 자식오브젝트. 활성화 타일 오브젝트들은 해당 오브젝트의 자식임
+    [SerializeField] private GameObject tileParentFalse; // 해당 오브젝트 내 자식오브젝트. 비활성화 타일 오브젝트들은 해당 오브젝트의 자식임
 
     private Transform tileParentTrans;  // tileParent의 transform
+    private Transform tileParentFalseTrans; // tileParentFalse의 transform
     private Transform tilePrefabTrans;  // 사전에 저장해둔 tile 오브젝트들을 자식으로 둔 오브젝트의 transform
 
     private bool isMakeMap = false; // 맵생성 함수가 호출되었는지 여부. true라면 더 호출할 수 없음
@@ -13,6 +15,7 @@ public class MapManager : MonoBehaviour
     void Awake()
     {
         tileParentTrans = tileParent.transform;
+        tileParentFalseTrans = tileParentFalse.transform;
     }
 
     // 준비된 오브젝트들과 주소를 통해 맵을 만듦
@@ -29,24 +32,19 @@ public class MapManager : MonoBehaviour
         // 오브젝트들을 가져옴
         while(tilePrefabTrans.childCount > 0)
         {
-            tilePrefabTrans.GetChild(0).parent = tileParentTrans;
+            tilePrefabTrans.GetChild(0).parent = tileParentFalseTrans;
         }
 
         for (int i = 0; i < 10; i++)
         {
             for (int k = 0; k < 15; k++)
             {
-				int index = 0;
-				for (int j = 0; j < tileParentTrans.childCount; j++)
-				{
-					if (!tileParentTrans.GetChild(j).gameObject.activeSelf)
-					{
-						index = j;
-						break;
-					}
+                if (tileParentFalseTrans.childCount > 0)
+                {
+					tileParentFalseTrans.GetChild(0).gameObject.SetActive(true);
+					tileParentFalseTrans.GetChild(0).localPosition = new Vector3(72 - (i * 16), -72 + (k * 16), 0f);
+                    tileParentFalseTrans.GetChild(0).parent = tileParentTrans;
 				}
-                tileParentTrans.GetChild(index).gameObject.SetActive(true);
-				tileParentTrans.GetChild(index).localPosition = new Vector3(72 - (i * 16), -72 + (k * 16), 0f);
 			}
 		}
 
