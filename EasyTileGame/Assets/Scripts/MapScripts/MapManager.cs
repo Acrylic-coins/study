8e3a40cdefad07095aeb33ab883336cf3df6f990
Rabbit49 <akasagarbha9@naver.com>
2025-08-01 15:01:09 +0900

    타일 생성 순서 변경

--- EasyTileGame/Assets/Scripts/MapScripts/MapManager.cs
+++ EasyTileGame/Assets/Scripts/MapScripts/MapManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,12 +11,17 @@ public class MapManager : MonoBehaviour
     private Transform tileParentFalseTrans; // tileParentFalse의 transform
     private Transform tilePrefabTrans;  // 사전에 저장해둔 tile 오브젝트들을 자식으로 둔 오브젝트의 transform
 
+    private WaitForSeconds term = new WaitForSeconds(0.05f);
+    private Coroutine firstCo;
+
     private bool isMakeMap = false; // 맵생성 함수가 호출되었는지 여부. true라면 더 호출할 수 없음
 
     void Awake()
     {
         tileParentTrans = tileParent.transform;
         tileParentFalseTrans = tileParentFalse.transform;
+
+        firstCo = null;
     }
 
     // 준비된 오브젝트들과 주소를 통해 맵을 만듦
@@ -35,18 +41,50 @@ public class MapManager : MonoBehaviour
             tilePrefabTrans.GetChild(0).parent = tileParentFalseTrans;
         }
 
-        for (int i = 0; i < 10; i++)
+        // 코루틴 호출 -> 타일을 텀을 두고 순차적으로 생성하기 위함
+		firstCo = StartCoroutine(SettingFirstTiles());
+
+    }
+
+    IEnumerator SettingFirstTiles()
+    {
+        int indX = 0;
+        int indY = 0;
+
+        // 대각방향 줄 개수 : 18개
+        for (int i = 0; i < 19; i++)
         {
-            for (int k = 0; k < 15; k++)
+            if (i < 10) { indX = i; indY = 0; }
+            else { indX = 9; indY = i - indX; }
+
+            // 대각선 방향에 있는 타일 개수 : 0 ~ 10개
+            for (int j = 0; j < (indX - indY) + 1; j++)
             {
                 if (tileParentFalseTrans.childCount > 0)
                 {
-					tileParentFalseTrans.GetChild(0).gameObject.SetActive(true);
-					tileParentFalseTrans.GetChild(0).localPosition = new Vector3(72 - (i * 16), -72 + (k * 16), 0f);
+                    tileParentFalseTrans.GetChild(0).gameObject.SetActive(true);
+                    tileParentFalseTrans.GetChild(0).localPosition = new Vector3(72 - ((indX - j) * 16), -72 + (((indY + j)) * 16), 0f);
                     tileParentFalseTrans.GetChild(0).parent = tileParentTrans;
-				}
-			}
+                }
+
+            }
+			yield return term;
 		}
 
+        // 화면 밖에 여분의 타일을 생성
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 10; j < 15; j++)
+            {
+                tileParentFalseTrans.GetChild(0).gameObject.SetActive(true);
+                tileParentFalseTrans.GetChild(0).localPosition = new Vector3(72 - (i * 16), -72 + (j * 16), 0f);
+                tileParentFalseTrans.GetChild(0).parent = tileParentTrans;
+            }
+        }
+
+        StopCoroutine(firstCo);
+
+
+        yield return null;
     }
 }
